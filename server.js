b10481d0bf20b4ad7d5fdb24622ce3cb7f127f0c
borosati85 <borosati85@gmail.com>
2019-09-01 22:18:53 +0200

    fixed console logging

--- server.js
+++ server.js
@@ -37,6 +37,4 @@ app.put('/image', (req, res) => {image.handleImage (req, res, database)});
 
 app.post('/imageURL', (req, res) => {image.handleApiCall (req, res)})
 
-app.listen(process.env.PORT || 3000,()=>{
-	console.log(`server is running on port ${PORT}`)
-});
+app.listen(process.env.PORT || 3000);
